213abd05c6664bfbee98f4bc37a150cb15e86a8f
fjcadrv <fjcadrv@gmail.com>
2026-03-20 06:37:50 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,15 +2,20 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-     <img 
-  src="/images/logo.png" 
-  alt="Mentalidad & Mercado FX"
-  className="w-32 h-32 mx-auto mb-2 object-contain"
-/>
-      <p className="text-4xl font-semibold text-center dark:text-white">
+    <header className="pt-16 pb-10 text-center">
+      
+      <div className="flex flex-col items-center mb-4">
+        <img 
+          src="/images/logo.png" 
+          alt="Mentalidad & Mercado FX"
+          className="w-32 h-32 object-contain mb-2"
+        />
+      </div>
+
+      <h1 className="text-4xl font-semibold text-white">
         <Link href="/">{name}</Link>
-      </p>
+      </h1>
+
     </header>
   );
 }
