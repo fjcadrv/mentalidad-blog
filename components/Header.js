21dfeef3c4192ff21871a491615386d143d5c721
fjcadrv <fjcadrv@gmail.com>
2026-03-20 06:09:54 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
-      <p className="text-2xl text-center dark:text-white">
+      <p className="text-4xl font-semibold text-center dark:text-white">
         <Link href="/">{name}</Link>
       </p>
     </header>
