5ced0b9ae3a52de08c08b16120a02d3488ca7103
fjcadrv <fjcadrv@gmail.com>
2026-03-20 06:30:55 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
      <img 
   src="/images/logo.png" 
   alt="Mentalidad & Mercado FX"
-  className="w-28 h-28 mx-auto mb-2 object-contain"
+  className="w-32 h-32 mx-auto mb-2 object-contain"
 />
       <p className="text-4xl font-semibold text-center dark:text-white">
         <Link href="/">{name}</Link>
